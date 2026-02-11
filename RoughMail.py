33f161f5a52131d0e898813ce639a9ccdf7c6475
Ashutosh Gunjal <ashuu8605@gmail.com>
2026-02-11 18:58:21 +0530

    first commit

---
+++ RoughMail.py
@@ -0,0 +1,61 @@
+import smtplib
+from email.message import EmailMessage
+
+
+
+def Send_Email(sender_email,app_password,recivers_email,subject,body):
+
+    msg=EmailMessage()
+
+
+    msg["From"]=sender_email
+    msg["To"]=recivers_email
+    msg["Subject"]=subject
+
+
+    #add mail body
+
+    msg.set_content(body)
+
+    smtp=smtplib.SMTP_SSL("smtp.gmail.com",465)
+
+
+    smtp.login(sender_email,app_password)
+
+    smtp.send_message(msg)
+
+
+    smtp.quit()
+
+
+
+    
+
+
+
+def main():
+    sender_email="ashutoshgunjal09@gmail.com"
+
+    app_password="pgpeookqyuystfea"
+
+
+    recivers_email="ashotosh_gunjal_it@moderncoe.edu.in"
+
+    subject="Test mail for python Script"
+
+    body="Jay ganesh" \
+    "This is a Script By Marvellous Infosystem " \
+    "Regards" \
+    "Ashutosh Gunjal"
+
+    Send_Email(sender_email,app_password,recivers_email,subject,body)
+
+
+    print("Marvelous mail Send Succesfully")
+
+
+    
+
+
+if __name__=="__main__":
+    main()
